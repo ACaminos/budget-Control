5fcfa4348b18a8d03f6711f3dad6f68a965a571d
Ariel <caminosariel@gmail.com>
2022-06-05 20:29:31 -0300

    add new dependencie in package.json, use react-swipeable-list

--- budget-control/src/components/Gasto.jsx
+++ budget-control/src/components/Gasto.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
+
 import { formatearFecha } from '../helpers';
+
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
 import IconoComida from '../img/icono_comida.svg'
@@ -19,9 +29,28 @@ const diccionarioIconos = {
 }
 
 const Gasto = ({gasto}) => {
-  const {categoria, nombre, cantidad, id, fecha } =gasto;
+  const {categoria, nombre, cantidad, id, fecha } = gasto;
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={ () => console.log('Editar...')}>
+        Editar
+      </SwipeAction>
+    </LeadingActions>
+  )
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={ () => console.log('Eliminar...')}>
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  )
   return (
-    <div className='gasto sombra'>
+    <SwipeableList>
+      <SwipeableListItem
+      leadingActions={leadingActions()}
+      trailingActions={trailingActions()}>
+        <div className='gasto sombra'>
       <div className='contenido-gasto'>
         <img
 
@@ -38,7 +67,9 @@ const Gasto = ({gasto}) => {
         </div>
       </div>
       <p className='cantidad-gasto'>${cantidad}</p>
-    </div>
+        </div>
+      </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
